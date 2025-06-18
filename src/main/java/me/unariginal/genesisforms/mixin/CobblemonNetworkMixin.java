c7f9140e50f4bc34ebb1ffe621b01bb3a7edb5ca
BuildTools <unconfigured@null.spigotmc.org>
2025-06-17 21:18:08 -0400

    GMG Compat

--- src/main/java/me/unariginal/genesisforms/mixin/CobblemonNetworkMixin.java
+++ src/main/java/me/unariginal/genesisforms/mixin/CobblemonNetworkMixin.java
@@ -6,6 +6,7 @@ import com.cobblemon.mod.common.net.PacketRegisterInfo;
 import com.cobblemon.mod.common.net.messages.client.pokemon.update.DmaxLevelUpdatePacket;
 import com.cobblemon.mod.common.net.messages.client.pokemon.update.GmaxFactorUpdatePacket;
 import com.llamalad7.mixinextras.sugar.Local;
+import net.fabricmc.loader.api.FabricLoader;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -21,22 +22,24 @@ import java.util.List;
 public class CobblemonNetworkMixin {
     @Inject(method = "generateS2CPacketInfoList", at = @At("RETURN"), remap = false)
     private void generateS2CPacketInfoList (CallbackInfoReturnable<List<PacketRegisterInfo<?>>> cir, @Local List<PacketRegisterInfo<?>> list) {
-        list.add(
-                new PacketRegisterInfo<>(
-                        DmaxLevelUpdatePacket.Companion.getID(),
-                        DmaxLevelUpdatePacket.Companion::decode,
-                        new PokemonUpdatePacketHandler<>(),
-                        null
-                )
-        );
+        if (!FabricLoader.getInstance().isModLoaded("gimme-that-gimmick")) {
+            list.add(
+                    new PacketRegisterInfo<>(
+                            DmaxLevelUpdatePacket.Companion.getID(),
+                            DmaxLevelUpdatePacket.Companion::decode,
+                            new PokemonUpdatePacketHandler<>(),
+                            null
+                    )
+            );
 
-        list.add(
-                new PacketRegisterInfo<>(
-                        GmaxFactorUpdatePacket.Companion.getID(),
-                        GmaxFactorUpdatePacket.Companion::decode,
-                        new PokemonUpdatePacketHandler<>(),
-                        null
-                )
-        );
+            list.add(
+                    new PacketRegisterInfo<>(
+                            GmaxFactorUpdatePacket.Companion.getID(),
+                            GmaxFactorUpdatePacket.Companion::decode,
+                            new PokemonUpdatePacketHandler<>(),
+                            null
+                    )
+            );
+        }
     }
 }
