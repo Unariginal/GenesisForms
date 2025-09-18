3e2544b292d64230c0b651f774b99aba4232dc65
Unariginal <nateabelson@gmail.com>
2025-09-18 14:27:34 -0400

    Denote old files, and properly update the main config file

--- src/main/java/me/unariginal/genesisforms/config/Config.java
+++ src/main/java/me/unariginal/genesisforms/config/Config.java
@@ -47,6 +47,16 @@ public class Config {
         if (!rootFolder.exists())
             rootFolder.mkdirs();
 
+        File oldAnimations = FabricLoader.getInstance().getConfigDir().resolve("GenesisForms/animations.json").toFile();
+        if (oldAnimations.exists()) {
+            oldAnimations.renameTo(FabricLoader.getInstance().getConfigDir().resolve("GenesisForms/old_animations.json").toFile());
+        }
+
+        File oldItemSettings = FabricLoader.getInstance().getConfigDir().resolve("GenesisForms/item_settings.json").toFile();
+        if (oldItemSettings.exists()) {
+            oldItemSettings.renameTo(FabricLoader.getInstance().getConfigDir().resolve("GenesisForms/old_item_settings.json").toFile());
+        }
+
         File configFile = FabricLoader.getInstance().getConfigDir().resolve("GenesisForms/config.json").toFile();
         JsonObject newRoot = new JsonObject();
         JsonObject root = new JsonObject();
@@ -58,33 +68,34 @@ public class Config {
         newRoot.addProperty("debug", debug);
 
         JsonObject generalSettings = new JsonObject();
+        JsonObject newGeneralSettings = new JsonObject();
         if (root.has("general_settings"))
             generalSettings = root.get("general_settings").getAsJsonObject();
 
         JsonObject keyItemSlots = new JsonObject();
-
+        JsonObject newKeyItemSlots = new JsonObject();
         if (root.has("key_item_slots"))
             keyItemSlots = root.get("key_item_slots").getAsJsonObject();
 
         if (keyItemSlots.has("hotbar"))
             useHotbarInventory = keyItemSlots.get("hotbar").getAsBoolean();
-        keyItemSlots.addProperty("hotbar", useHotbarInventory);
+        newKeyItemSlots.addProperty("hotbar", useHotbarInventory);
 
         if (keyItemSlots.has("main"))
             useMainInventory = keyItemSlots.get("main").getAsBoolean();
-        keyItemSlots.addProperty("main", useMainInventory);
+        newKeyItemSlots.addProperty("main", useMainInventory);
 
         if (keyItemSlots.has("mainhand"))
             useMainHandInventory = keyItemSlots.get("mainhand").getAsBoolean();
-        keyItemSlots.addProperty("mainhand", useMainHandInventory);
+        newKeyItemSlots.addProperty("mainhand", useMainHandInventory);
 
         if (keyItemSlots.has("offhand"))
             useOffHandInventory = keyItemSlots.get("offhand").getAsBoolean();
-        keyItemSlots.addProperty("offhand", useOffHandInventory);
+        newKeyItemSlots.addProperty("offhand", useOffHandInventory);
 
         if (keyItemSlots.has("armor"))
             useArmorInventory = keyItemSlots.get("armor").getAsBoolean();
-        keyItemSlots.addProperty("armor", useArmorInventory);
+        newKeyItemSlots.addProperty("armor", useArmorInventory);
 
         if (keyItemSlots.has("specific")) {
             JsonArray specificSlots = keyItemSlots.get("specific").getAsJsonArray();
@@ -96,9 +107,9 @@ public class Config {
         for (int slot : this.specificSlots) {
             specificSlots.add(slot);
         }
-        keyItemSlots.add("specific", specificSlots);
+        newKeyItemSlots.add("specific", specificSlots);
 
-        generalSettings.add("key_item_slots", keyItemSlots);
+        newGeneralSettings.add("key_item_slots", newKeyItemSlots);
 
         JsonArray disabledItems = new JsonArray();
         if (generalSettings.has("disabled_items")) {
@@ -108,92 +119,97 @@ public class Config {
         for (String item : this.disabledItems) {
             disabledItems.add(item);
         }
-        generalSettings.add("disabled_items", disabledItems);
+        newGeneralSettings.add("disabled_items", disabledItems);
 
-        newRoot.add("general_settings", generalSettings);
+        newRoot.add("general_settings", newGeneralSettings);
 
         JsonObject megaSettings = new JsonObject();
+        JsonObject newMegaSettings = new JsonObject();
         if (root.has("mega_settings"))
             megaSettings = root.get("mega_settings").getAsJsonObject();
 
         if (megaSettings.has("enable_mega_evolution"))
             enableMegaEvolution = megaSettings.get("enable_mega_evolution").getAsBoolean();
-        megaSettings.addProperty("enable_mega_evolution", enableMegaEvolution);
+        newMegaSettings.addProperty("enable_mega_evolution", enableMegaEvolution);
 
         if (megaSettings.has("allow_mega_outside_battles"))
             allowMegaOutsideBattles = megaSettings.get("allow_mega_outside_battles").getAsBoolean();
-        megaSettings.addProperty("allow_mega_outside_battles", allowMegaOutsideBattles);
+        newMegaSettings.addProperty("allow_mega_outside_battles", allowMegaOutsideBattles);
 
-        newRoot.add("mega_settings", megaSettings);
+        newRoot.add("mega_settings", newMegaSettings);
 
         JsonObject zPowerSettings = new JsonObject();
+        JsonObject newZPowerSettings = new JsonObject();
         if (root.has("z_power_settings"))
             zPowerSettings = root.get("z_power_settings").getAsJsonObject();
 
         if (zPowerSettings.has("enable_z_crystals"))
             enableZCrystals = zPowerSettings.get("enable_z_crystals").getAsBoolean();
-        zPowerSettings.addProperty("enable_z_crystals", enableZCrystals);
+        newZPowerSettings.addProperty("enable_z_crystals", enableZCrystals);
 
-        newRoot.add("z_power_settings", zPowerSettings);
+        newRoot.add("z_power_settings", newZPowerSettings);
 
         JsonObject teraSettings = new JsonObject();
+        JsonObject newTeraSettings = new JsonObject();
         if (root.has("tera_settings"))
             teraSettings = root.get("tera_settings").getAsJsonObject();
 
         if (teraSettings.has("enable_tera"))
             enableTera = teraSettings.get("enable_tera").getAsBoolean();
-        teraSettings.addProperty("enable_tera", enableTera);
+        newTeraSettings.addProperty("enable_tera", enableTera);
 
         if (teraSettings.has("tera_shards_required"))
             teraShardsRequired = teraSettings.get("tera_shards_required").getAsInt();
         if (teraShardsRequired > 64) teraShardsRequired = 64;
         if (teraShardsRequired < 1) teraShardsRequired = 1;
-        teraSettings.addProperty("tera_shards_required", teraShardsRequired);
+        newTeraSettings.addProperty("tera_shards_required", teraShardsRequired);
 
         if (teraSettings.has("consume_tera_shards"))
             consumeTeraShards = teraSettings.get("consume_tera_shards").getAsBoolean();
-        teraSettings.addProperty("consume_tera_shards", consumeTeraShards);
+        newTeraSettings.addProperty("consume_tera_shards", consumeTeraShards);
 
         if (teraSettings.has("require_orb_recharge"))
             requireOrbRecharge = teraSettings.get("require_orb_recharge").getAsBoolean();
-        teraSettings.addProperty("require_orb_recharge", requireOrbRecharge);
+        newTeraSettings.addProperty("require_orb_recharge", requireOrbRecharge);
 
         if (teraSettings.has("fix_ogerpon_tera_type"))
             fixOgerponTeraType = teraSettings.get("fix_ogerpon_tera_type").getAsBoolean();
-        teraSettings.addProperty("fix_ogerpon_tera_type", fixOgerponTeraType);
+        newTeraSettings.addProperty("fix_ogerpon_tera_type", fixOgerponTeraType);
 
         if (teraSettings.has("fix_terapagos_tera_type"))
             fixTerapagosTeraType = teraSettings.get("fix_terapagos_tera_type").getAsBoolean();
-        teraSettings.addProperty("fix_terapagos_tera_type", fixTerapagosTeraType);
+        newTeraSettings.addProperty("fix_terapagos_tera_type", fixTerapagosTeraType);
 
-        newRoot.add("tera_settings", teraSettings);
+        newRoot.add("tera_settings", newTeraSettings);
 
         JsonObject fusionSettings = new JsonObject();
+        JsonObject newFusionSettings = new JsonObject();
         if (root.has("fusion_settings"))
             fusionSettings = root.get("fusion_settings").getAsJsonObject();
 
         if (fusionSettings.has("enable_fusions"))
             enableFusions = fusionSettings.get("enable_fusions").getAsBoolean();
 
-        fusionSettings.addProperty("enable_fusions", enableFusions);
+        newFusionSettings.addProperty("enable_fusions", enableFusions);
 
-        newRoot.add("fusion_settings", fusionSettings);
+        newRoot.add("fusion_settings", newFusionSettings);
 
         JsonObject dynamaxSettings = new JsonObject();
+        JsonObject newDynamaxSettings = new JsonObject();
         if (root.has("dynamax_settings"))
             dynamaxSettings = root.get("dynamax_settings").getAsJsonObject();
 
         if (dynamaxSettings.has("enable_dynamax"))
             enableDynamax = dynamaxSettings.get("enable_dynamax").getAsBoolean();
 
-        dynamaxSettings.addProperty("enable_dynamax", enableDynamax);
+        newDynamaxSettings.addProperty("enable_dynamax", enableDynamax);
 
         if (dynamaxSettings.has("enable_gigantamax"))
             enableGigantamax = dynamaxSettings.get("enable_gigantamax").getAsBoolean();
 
-        dynamaxSettings.addProperty("enable_gigantamax", enableGigantamax);
+        newDynamaxSettings.addProperty("enable_gigantamax", enableGigantamax);
 
-        newRoot.add("dynamax_settings", dynamaxSettings);
+        newRoot.add("dynamax_settings", newDynamaxSettings);
 
         configFile.delete();
         configFile.createNewFile();
